151f686cb627a28e5ba79152f916083ab20688cf
Tanmay Khairnar <tanmayska11@gmail.com>
2023-09-07 23:07:09 +0530

    init push

--- src/main/java/com/spring/project1/controllers/CustomerController.java
+++ src/main/java/com/spring/project1/controllers/CustomerController.java
@@ -2,23 +2,15 @@ package com.spring.project1.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.spring.project1.payloads.ApiResponse;
 import com.spring.project1.payloads.CustomerDto;
 import com.spring.project1.services.CustomerService;
 
@@ -40,7 +32,7 @@ public class CustomerController {
 		List<CustomerDto> customers = customerService.getAllCustomers();		
 		
 		model.addAttribute("customers",customers);
-		//return ResponseEntity.ok(this.customerService.getAllCustomers());		
+			
 		return "index";
 	}
 	
@@ -49,7 +41,7 @@ public class CustomerController {
 		@PostMapping("/addCustomer")
 		public String createCustomer(@Validated @ModelAttribute("customerDto") CustomerDto customerDto){
 			CustomerDto createCustomerDto =this.customerService.createCustomer(customerDto);
-		//	return new ResponseEntity<>(createCustomerDto,HttpStatus.CREATED);
+		
 			return "redirect:/api/customers/";
 		}
 		
@@ -60,21 +52,7 @@ public class CustomerController {
 		    return "addCustomer";
 		}
 		
-		
-		
-
-
-	
-	
-	//post-create Customer
-//	@PostMapping("/")
-//	public ResponseEntity<CustomerDto> createCustomer(@Validated @RequestBody CustomerDto customerDto){
-//		CustomerDto createCustomerDto =this.customerService.createCustomer(customerDto);
-//		return new ResponseEntity<>(createCustomerDto,HttpStatus.CREATED);
-//		
-//	}
-		
-		
+				
 		@PostMapping("/update")
 		public String updateCustomer( @Validated @ModelAttribute("customerDto") CustomerDto customerDto){
 			CustomerDto updatedCustomer =this.customerService.updateCustomer(customerDto);
@@ -83,20 +61,7 @@ public class CustomerController {
 			
 		}
 		
-//		@GetMapping("/update")
-//		public String showUpdateCustomerForm(Model model) {
-//		    model.addAttribute("customerDto", new CustomerDto());
-//		    return "updateCustomer";
-//		}
-	
-//	//put -update Customer
-//	
-//	@PutMapping("/{customerId}")
-//	public ResponseEntity<CustomerDto> updateCustomer( @Validated @RequestBody CustomerDto customerDto ,@PathVariable String customerId){
-//		CustomerDto updatedCustomer =this.customerService.updateCustomer(customerDto, customerId);
-//		return new ResponseEntity<>(updatedCustomer,HttpStatus.OK);
-//		
-//	}
+
 	
 	//delete -delete Customer
 		@PostMapping("/delete")
@@ -108,30 +73,7 @@ public class CustomerController {
 			return "redirect:/api/customers/"; 
 		}
 	
-//	//delete -delete Customer
-//	@DeleteMapping("/{customerId}")
-//	public ResponseEntity<ApiResponse> deleteCustomer( @PathVariable String customerId){
-//		
-//		this.customerService.deleteCustomer(customerId);
-//		
-//		return new ResponseEntity<ApiResponse>(new ApiResponse("message Customer deleted sucessfully",true),HttpStatus.OK);
-//		
-//	}
 	
-//	//get =All Customer get
-//	@GetMapping("/")
-//	public ResponseEntity<List<CustomerDto>> getAllCustomers(){
-//		return ResponseEntity.ok(this.customerService.getAllCustomers());		
-//		
-//	}
-	
-//	//get single Customer
-//	@GetMapping("/{customerId}")
-//	public ResponseEntity<CustomerDto> getAllCustomers(@PathVariable String customerId){
-//		return ResponseEntity.ok(this.customerService.getCustomerById(customerId));
-//		
-//	}
-		
 		
 		@GetMapping("/update/{customerId}")
 		public String showUpdateCustomerForm(@PathVariable String customerId,Model model){
